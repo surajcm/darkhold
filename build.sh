38b74b2a6a104f5fb53af271afb26052d5f7a01e
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2020-12-04 09:13:19 +0530

    Updating to java 15 and gradle 6.7.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/openjdk-14.0.2.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-15.0.1.jdk/Contents/Home"
 
 #export JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64/"
 
