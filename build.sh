3b04d8af2baae8e3a0f7f6064f51cfebd2ae8758
Suraj Muraleedharan <v-smuraleedharan@expediagroup.com>
2021-05-02 22:35:24 +0530

    support for delete on web page - functionality is yet to implement, updated to java 16, updated circle ci configuration

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-15.0.1.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-16.0.1.jdk/Contents/Home"
 
 #export JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64/"
 
