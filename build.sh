a8cf86aa7479aedc499271b3980013c5eacf4ce1
Suraj Muraleedharan <surajcm@gmail.com>
2023-02-09 15:22:16 +0530

    Upgrading to java 19

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-18.jdk/Contents/Home"
-#export JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64/"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-19.jdk/Contents/Home"
 export JAVA_OPTS="-Xms512m -Xmx512m"
 exec ./gradlew "$@"
